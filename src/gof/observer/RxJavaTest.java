841c4c26f9051c3e32c976c080ca19fbcbc07208
Juan Pablo Vergara <juanpavergara@Juans-MacBook-Air.local>
2015-03-05 07:38:55 -0500

    ReactiveList example added using PublishSubject

--- src/gof/observer/RxJavaTest.java
+++ src/gof/observer/RxJavaTest.java
@@ -1,5 +1,6 @@
 package gof.observer;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -7,9 +8,14 @@ import rx.functions.Action1;
 
 public class RxJavaTest {
 	
+	public RxJavaTest(){
 	
-	public static void hello(String... names) {
+	}
 	
+	public void hello(String... names) {
+		
+		// Se crea un Observable a partir de una estructura de datos.
+		// Se suscribe un observador con Action1
 	    Observable.from(names).subscribe(new Action1<String>() {
 	    	@Override
 	        public void call(String s) {
@@ -19,10 +25,12 @@ public class RxJavaTest {
 	    	    
 	}
 	
-	public static void interval(){
+	public void interval(){
 		
+		// Crea un observable que emite eventos cada unidad de tiempo establecida
 		Observable<Long> obs = Observable.interval(1, TimeUnit.SECONDS);
 		
+		// Se suscribe un observador con Action1
 		obs.subscribe(new Action1<Long>() {
 	    	@Override
 	        public void call(Long s) {
@@ -33,9 +41,11 @@ public class RxJavaTest {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		hello("Juan", "Pablo");
+		RxJavaTest test = new RxJavaTest();
 		
-		interval();
+		test.hello("Juan", "Pablo", "Clase", "Frameworks", "Patrones");
+		
+		test.interval();		
 		
 		Thread.sleep(10000);
 		
