e3c5ef924042072b22d6e89566e6ffe9cf921497
Juan Pablo Vergara <juanpavergara@Juans-MacBook-Air.local>
2015-03-05 07:42:44 -0500

    Publishing example for ObservableList using composition over inheritance with the List type and the PublishSubject type

---
+++ src/gof/observer/ObservableListExample.java
@@ -0,0 +1,39 @@
+package gof.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import rx.Observable;
+import rx.subjects.PublishSubject;
+
+public class ObservableListExample {
+	
+    public static class ObservableList<T> {
+        
+    	protected final List<T> list;
+        protected final PublishSubject<T> onAdd;
+        
+        public ObservableList() {
+            this.list = new ArrayList<T>();
+            this.onAdd = PublishSubject.create();
+        }
+        public void add(T value) {
+            list.add(value);
+            onAdd.onNext(value);
+        }
+        public Observable<T> getObservable() {
+            return onAdd;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        ObservableList<Integer> olist = new ObservableList<>();
+        
+        olist.getObservable().subscribe(System.out::println);
+        
+        olist.add(1);
+        Thread.sleep(1000);
+        olist.add(2);
+        Thread.sleep(1000);
+        olist.add(3);
+    }
+}
